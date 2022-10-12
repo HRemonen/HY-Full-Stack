5badfd8f38f8f07d1ef5551d94f3059bc5eb10d2
Henri Remonen <henri@remonen.fi>
2022-10-12 15:54:54 +0300

    Viikko 1 tehtävä 1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -11,17 +11,27 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [totalClicks, setTotalClick] = useState(0)
+  const [totalValue, setTotalValue] = useState(0)
+
+  const increaseTotalClick = () => setTotalClick(totalClicks + 1)
+  const alterTotalValue = (value) => setTotalValue(totalValue + value)
 
   const positiveFeedback = () => {
     setGood(good + 1)
+    increaseTotalClick()
+    alterTotalValue(1)
   }
 
   const neutralFeedback = () => {
     setNeutral(neutral + 1)
+    increaseTotalClick()
   }
 
   const badFeedback = () => {
     setBad(bad + 1)
+    increaseTotalClick()
+    alterTotalValue(-1)
   }
 
   return (
@@ -35,6 +45,9 @@ const App = () => {
       <Display text={"Good: "} value={good}></Display>
       <Display text={"Neutral: "} value={neutral}></Display>
       <Display text={"Bad: "} value={bad}></Display>
+      <Display text={"All: "} value={totalClicks}></Display>
+      <Display text={"Average: "} value={totalValue / totalClicks}></Display>
+      <Display text={"Positive: "} value={((good / totalClicks) * 100) + " %" }></Display>
     </div>
   )
 }
