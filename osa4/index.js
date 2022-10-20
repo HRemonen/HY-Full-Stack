ac3179a9b6b013423bec931532b28eaac5629f7d
Henri Remonen <henri@remonen.fi>
2022-10-20 16:22:22 +0300

    Viikko 4.2 refactoring

--- osa4/index.js
+++ osa4/index.js
@@ -1,69 +1,11 @@
-require('dotenv').config()
-
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-const cors = require('cors')
+const server = http.createServer(app)
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const body = request.body
-
-  if (!body.title || !body.author || !body.url) {
-    return reponse.status(400).json({
-        error: 'Blog post mandatory info missing!'
-    })
-  }
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: 0
-  })
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
-})
-
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  app.use(unknownEndpoint)
-  
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    }
-    else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
-    }
-    next(error)
-  }
-  
-  app.use(errorHandler)
   
--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -12,10 +12,28 @@ mongoose.connect(url)
 })
 
 const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
+    title: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    },
     likes: Number
+})
+
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
