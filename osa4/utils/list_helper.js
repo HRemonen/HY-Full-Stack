20332058d69fc6baac040dcf099e6863e563b007
Henri Remonen <henri@remonen.fi>
2022-10-22 10:31:15 +0300

    Viikko 4 tehtävä 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,9 +1,16 @@
-
-
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((total, value) => {
+        console.log('Total', total)
+        return total + value.likes
+    }, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
+
