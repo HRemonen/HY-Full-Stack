a9174a3bc3ce8dc4c41d4c8b41772942e32ffe92
Henri Remonen <henri@remonen.fi>
2022-10-22 13:49:22 +0300

    Viikko 4 tehtävä 4.7

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -17,11 +17,17 @@ const favoriteBlog = (blogs) => {
     })[0]
 }
 
-const mostBlogs = (blogs) => {
-    authors = new Map()
+const getMax = (authors, value) => {
+    const authorWithTheMost = [...authors.entries()].reduce((a, b) => b[1] > a[1] ? b : a)
 
+    return {author: authorWithTheMost[0],
+            [value]: authorWithTheMost[1]}
+}
+
+const mostBlogs = (blogs) => {
     if (!blogs || blogs.length === 0) return undefined
 
+    authors = new Map()
     blogs.map(o => {
         if (authors.has(o.author)) {
             authors.set(o.author, authors.get(o.author) + 1)
@@ -31,77 +37,29 @@ const mostBlogs = (blogs) => {
         }
     })
 
-    const authorWithMostPosts = [...authors.entries()].reduce((a, b) => b[1] > a[1] ? b : a)
+    return getMax(authors, "blogs")
+}
 
-    return {author: authorWithMostPosts[0],
-            blogs: authorWithMostPosts[1]}
+const mostLikes = (blogs) => {
+    if (!blogs || blogs.length === 0) return undefined
+
+    authors = new Map()
+    blogs.map(o => {
+        if (authors.has(o.author)) {
+            authors.set(o.author, authors.get(o.author) + o.likes)
+        }
+        else {
+            authors.set(o.author, o.likes)
+        }
+    })
+
+    return getMax(authors, "likes")
 }
 
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
   }
-
-
-const biggerListOfBlogs = [
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
-    },
-    {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0
-    },
-    {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0
-    }  
-]
-
-console.log(mostBlogs([    {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  }]))
-
