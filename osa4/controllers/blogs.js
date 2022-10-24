909474ab263620e185023cec3016cb6ba2757b71
Henri Remonen <henri@remonen.fi>
2022-10-24 15:31:49 +0300

    Viikko 4 tehtävä 4.17

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', {username: 1, name: 1})
 
   response.json(blogs)
 })
@@ -11,9 +14,9 @@ blogsRouter.put('/:id', async (request, response) => {
   const body = request.body
 
   const blog = {
+    url: body.url,
     title: body.title,
     author: body.author,
-    url: body.url,
     likes: body.likes
   }
 
@@ -25,6 +28,8 @@ blogsRouter.put('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.user) || await User.findOne()
+
   if (!body.title || !body.url) {
     return response.status(400).json({
       error: 'Blog post mandatory info missing!'
@@ -32,13 +37,16 @@ blogsRouter.post('/', async (request, response) => {
   }
 
   const blog = new Blog({
-    title: body.title,
-    author: body.author,
     url: body.url,
+    title: body.title,
+    author: body.author || undefined,
+    user: user.id,
     likes: body.likes || 0
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog.id)
+  await user.save()
 
   response.status(201).json(savedBlog)
 })
--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose')
 
 const blogSchema = mongoose.Schema({
+  url: {
+    type: String,
+    required: true
+  },
   title: {
     type: String,
     required: true
   },
   author: {
-    type: String,
+    type: String
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
-  },
-  url: {
-    type: String,
-    required: true
   },
   likes: Number
 })
