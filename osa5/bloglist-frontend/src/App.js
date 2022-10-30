1b01ef1928dd1e777813e0cd8ee62c6ec9ae7592
Henri Remonen <henri@remonen.fi>
2022-10-30 15:19:51 +0200

    Viikko 5 tehtävä 5.1 & 5.2

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,9 +1,20 @@
 import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import RenderBlogs from './components/RenderBlogs'
+import Logout from './components/Logout'
+
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [message, setMessage] = useState(null)
+  const [messageType, setMessageType] = useState(null)
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +22,69 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleUsernameInput = async (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordInput = async (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log(`Logging in user ${username}`)
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+    catch (exception) {
+      console.log('Wrong credentials!')
+    }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+    setUsername('')
+    setPassword('')
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null && 
+        <LoginForm
+          username={username}
+          password={password}
+          handleLogin={handleLogin}
+          handleUsernameInput={handleUsernameInput}
+          handlePasswordInput={handlePasswordInput} 
+        />}
+
+      {!(user === null) &&
+        <>
+          <Logout user={user} handleLogout={handleLogout} />
+          <RenderBlogs
+            user={user}
+            blogs={blogs}
+          />
+          </>}
     </div>
   )
 }
