56ec9950f54679f794a021c3eeb9a164ae45f166
Henri Remonen <henri@remonen.fi>
2022-10-30 15:31:13 +0200

    Add requestlogger middleware to the backend

--- osa5/bloglist-server/app.js
+++ osa5/bloglist-server/app.js
@@ -8,10 +8,10 @@ require('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-const { errorHandler, userExtractor } = require('./utils/middleware')
+const { errorHandler, userExtractor, requestLogger } = require('./utils/middleware')
 const logger = require('./utils/logger')
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info('connecting to mongodb')
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -24,6 +24,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(requestLogger)
 
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', userExtractor, blogsRouter)
