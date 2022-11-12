1fbc2e88ca0c2d05c576324da50fba04c08337b8
Henri Remonen <henri@remonen.fi>
2022-11-12 12:11:11 +0200

    Viikko 5 tehtävät 5.17 & 5.18

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,13 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    const user = {
+      name: 'Henri Remonen',
+      username: 'Hene',
+      password: 'salasana'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -8,11 +15,26 @@ describe('Blog app', function() {
     cy.contains('Log in to blogs')
   })
 
-  it('user can log in', function() {
-    cy.get('#username').type('Hene')
-    cy.get('#password').type('salasana')
-    cy.get('#login-button').click()
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('Hene')
+      cy.get('#password').type('salasana')
+      cy.get('#login-button').click()
 
-    cy.contains('Logged in as user: Henri Remonen')
+      cy.get('.success-msg').should('contain', 'Logged in as user: Henri Remonen')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('Hene')
+      cy.get('#password').type('123123')
+      cy.get('#login-button').click()
+
+      cy.get('.error-msg').should('contain', 'Wrong username or password')
+      cy.get('.error-msg').should('have.css', 'color', 'rgb(216, 0, 12)')
+    })
+  })
+
+  describe('when logged in', function() {
+    //
   })
 })
