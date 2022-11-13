4454d63b49eedac367ce5095e48d5f198bc3c614
Henri Remonen <henri@remonen.fi>
2022-11-13 11:58:26 +0200

    Anecdote reducer now handles anectode creation and voting as its own

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -24,6 +24,17 @@ export const voteAnectode = (id) => {
   }
 }
 
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: {
+      content,
+      id: getId(),
+      votes: 0
+    }
+  }
+}
+
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
@@ -42,8 +53,11 @@ const reducer = (state = initialState, action) => {
       return state.map(anecdote => {
         return anecdote.id !== id ? anecdote : changedAnecdote
       })
+    
+    case 'NEW_ANECDOTE' :
+      return [...state, action.data]
 
-    default: return state
+    default: return state.map(anecdote)
   }
 }
 
