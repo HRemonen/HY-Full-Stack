a9f5cd8979ae0b9eaf856ef74fffb75ae9fb83db
Henri Remonen <henri@remonen.fi>
2022-11-19 09:52:52 +0200

    Remove old functions from anecdote reducer

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -2,34 +2,6 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-/*
-export const voteAnectode = (id) => {
-  return {
-    type: 'VOTE',
-    data: { id }
-  }
-}
-
-export const createAnecdote = (content) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    data: {
-      content,
-      id: getId(),
-      votes: 0
-    }
-  }
-}
-*/
-
 const initialState = []
 
 const anecdoteSlice = createSlice({
@@ -57,38 +29,16 @@ const anecdoteSlice = createSlice({
         id: getId(),
         votes: 0
       })
+    },
+    appendAnecdote(state, action) {
+      state.push(action.payload)
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
     
   }
 })
 
-/*
-const anecdoteReducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-  
-  switch(action.type) {
-    case 'VOTE' :
-      const id = action.data.id
-      const anecdoteToChange = state.find(a => a.id === id)
-      const changedAnecdote = {
-        ...anecdoteToChange,
-        votes: anecdoteToChange.votes + 1
-      }
-
-      return state.map(anecdote => {
-        return anecdote.id !== id ? anecdote : changedAnecdote
-      })
-    
-    case 'NEW_ANECDOTE' :
-      return [...state, action.data]
-
-    default: return state
-  }
-}
-*/
-
-// export default anecdoteReducer
-
-export const { voteAnectode, createAnecdote } = anecdoteSlice.actions
+export const { voteAnectode, createAnecdote, appendAnecdote, setAnecdotes} = anecdoteSlice.actions
 export default anecdoteSlice.reducer
