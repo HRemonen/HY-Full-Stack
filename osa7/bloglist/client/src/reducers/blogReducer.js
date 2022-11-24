f3ad1f52286f50aa2a3818752a3f64f94414034f
Henri Remonen <henri@remonen.fi>
2022-11-24 10:58:01 +0200

    New action creators to communicate in app

--- osa7/bloglist/client/src/reducers/blogReducer.js
+++ osa7/bloglist/client/src/reducers/blogReducer.js
@@ -1,16 +1,64 @@
 import { createSlice } from '@reduxjs/toolkit'
-import blogService from "./services/blogs";
+import blogService from "../services/blogs";
 
 const initialState = []
 
 const blogSlice = createSlice({
-  name: 'blog',
+  name: 'blogs',
   initialState,
   reducers: {
-    
-    
+    like(state, action) {
+      const id = action.payload.id
+
+      const blogToLike = state.find(b => b.id === id)
+
+      const likedBlog = {
+        ...blogToLike,
+        likes: blogToLike.likes + 1
+      }
+
+      return state.map(blog => {
+        return blog.id !== id ? blog : likedBlog
+      })
+    },
+    delete(state, action) {
+      return
+    },
+    appendBlogs(state, action) {
+      state.push(action.payload)
+    },
+    setBlogs(state, action) {
+      return action.payload
+    }
   }
 })
 
+export const { like, appendBlogs, setBlogs } = blogSlice.actions
+
+export const initializeBlogs = () => {
+  return async dispatch => {
+    const blogs = await blogService.getAll()
+   
+    dispatch(setBlogs(blogs))
+  }
+}
+
+export const createBlog = (content) => {
+  return async dispatch => {
+    const newBlog = await blogService.create(content)
+    dispatch(appendBlogs(newBlog))
+  }
+}
+
+export const likeBlog = (content) => {
+  return async dispatch => {
+    const blog = await blogService.update(content)
+    dispatch(like(blog))
+  }
+}
+
+export const deleteBlog = () => {
+
+}
 
 export default blogSlice.reducer
