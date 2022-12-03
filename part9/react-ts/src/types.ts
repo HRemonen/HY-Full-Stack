217c4cb181cfac74b86e1c3405efc7bd9bd3ea6b
Henri Remonen <henri@remonen.fi>
2022-12-03 10:17:20 +0200

    Modify the types.ts to implement different possible part types

--- part9/react-ts/src/types.ts
+++ part9/react-ts/src/types.ts
@@ -1,11 +1,31 @@
-interface CourseParts {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
+  type: string;
 }
+
+interface CourseDescriptionPart extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CourseDescriptionPart {
+  type: "normal";
+}
+
+interface CourseSubmissionPart extends CourseDescriptionPart {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+export type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
 
 interface Parts {
-  parts: Array<CourseParts>
+  parts: Array<CoursePart>
 }
 
-
-export type { CourseParts, Parts }
+export type { Parts };
