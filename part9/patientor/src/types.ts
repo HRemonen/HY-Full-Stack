4a649e823118f1fd1476a0c8511fcf0aa8c20c6a
Henri Remonen <henri@remonen.fi>
2022-12-05 17:15:29 +0200

    Add Union type for new entries

--- part9/patientor/src/types.ts
+++ part9/patientor/src/types.ts
@@ -65,3 +65,8 @@ export interface Patient {
   dateOfBirth?: string;
   entries: Array<Entry>
 }
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+export type NewEntry = UnionOmit<Entry, 'id'>;
